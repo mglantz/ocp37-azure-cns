34974552b3cc80201ae950a8e608658e5f31d1ed
Magnus Glantz <sudo@redhat.com>
2017-12-04 19:13:50 +0100

    3.7

--- scripts/bastionPrep.sh
+++ scripts/bastionPrep.sh
@@ -63,7 +63,7 @@ subscription-manager repos \
     --enable="rhel-7-server-rpms" \
     --enable="rhel-7-server-extras-rpms" \
     --enable="rhel-7-fast-datapath-rpms" \
-    --enable="rhel-7-server-ose-3.6-rpms"
+    --enable="rhel-7-server-ose-3.7-rpms"
 
 # Install base packages and update system to latest packages
 echo $(date) " - Install base packages and update system to latest packages"
--- scripts/nodePrep.sh
+++ scripts/nodePrep.sh
@@ -63,7 +63,7 @@ subscription-manager repos \
     --enable="rhel-7-server-rpms" \
     --enable="rhel-7-server-extras-rpms" \
     --enable="rhel-7-fast-datapath-rpms" \
-    --enable="rhel-7-server-ose-3.6-rpms"
+    --enable="rhel-7-server-ose-3.7-rpms"
 
 # Install and enable Cockpit
 echo $(date) " - Installing and enabling Cockpit"
